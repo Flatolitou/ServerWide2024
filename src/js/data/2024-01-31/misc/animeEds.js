b6c7fe870bcc4d0784d0e83682412f81c2b7f018
Flatolitou <115586273+Flatolitou@users.noreply.github.com>
2024-09-04 15:31:13 +0200

    Update animeEds.js

--- src/js/data/2024-01-31/misc/animeEds.js
+++ src/js/data/2024-01-31/misc/animeEds.js
@@ -1,207 +1,7 @@
 export const animeEndings = [
   {
-    name: "Punishing Gray Raven - Under Dimension feat. Kinoko by CoyDe",
-    videoId: "fzlQmQ1Eesk",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Soulstone Survivor - \"The Dungeon of Despair\" by Guilherme Gama",
-    videoId: "IN1Sp9uEIK8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "PAYDAY 3 - \"Heatwave\" by Gustavo Coutinho",
-    videoId: "eDcVkzqpio4",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Shin Megami Tensei IV - \"Battle B2\" by Ryota Kozuka",
-    videoId: "A6cSbof7Pik",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Evergrace II - \"Forgotten Valley, Sanctuary\" by Kota Hoshino",
-    videoId: "mziO5bHQM5I",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Mega Man II - \"Dr. Wily Stage 1\" by Takashi Tateishi",
-    videoId: "aTbfpkByIM8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Jump King - \"Antumbra\" by Nils Ekl\u00f6f and Elias Th\u00f6rnlund",
-    videoId: "n22Yv0Q-S_8",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Destiny Child - More Beautiful, More Painful by Mitsukiyo, Seibin",
-    videoId: "bIsdi51sxF4",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "F-Zero X - \"Dream Chaser\" by Taro Bando",
-    videoId: "EUSxabS5_qM",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Dark Souls II - \"Longing\" by Motoi Sakuraba",
-    videoId: "SjlvApK2YzA",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Rabbit and Steel - \"A Valiant Battle\" by HAGANE",
-    videoId: "jBRkklVhIgw",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Shenmue - \"Memories of Distant Days\" by Osamu Murata",
-    videoId: "n-1JtMAWjAU",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "MapleStory - \"Start The Adventure\" by LedZepper",
-    videoId: "8-uZ-l1Hdb4",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Fate/Grand Order - \"True Human\" by Keita Haga",
-    videoId: "yhRE3U4j1B4",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "A Hat in Time - \"Your Contract Has Expired\" by Pascal Michael Stiefel",
-    videoId: "BePP156LQgY",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Elden Ring: Shadow of the Erdtree - \"Messmer, the Impaler\" by Tsukasa Saitoh",
-    videoId: "dylyeeZQ4Hw",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "eXceed 3rd - Jade Penetrate Black Package - \"Fierce Fight - Remix\" by S.S.H.",
-    videoId: "ov-PK4YpQXc",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Crystalis - \"Mt. Sabre\" by Yoko Osaka",
-    videoId: "_5yOhkrhY88",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Darksiders Genesis - \"Astarte.\" by Gareth Coker",
-    videoId: "0lGHMyprOeQ",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Genshin Impact - Her Wishes by HOYO-MiX",
-    videoId: "NKZvfX65Y9k",
-    videoSource: VIDEOSOURCE.YOUTUBE,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Pokemon Reborn - \"Dark Crystal\" by GlitchxCity",
-    videoId: "https://mega.nz/folder/CgUTDCYC#UnICsrlk1PJN4_GWr3eNUg/file/ShVmGSCa",
+    name: "We Without Wings \u2013 Under the Innocent Sky - \"Spread Wings.\" by Aki Misato",
+    videoId: "https://ladist1.catbox.video/d1wnb3.mp3",
     videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
@@ -210,9 +10,9 @@ export const animeEndings = [
     }
   },
   {
-    name: "Space Channel 5 - \"Mexican Flyer\" by Ken Woodman\u2019s Piccadilly Brass",
-    videoId: "r8hAtMJSaDg",
-    videoSource: VIDEOSOURCE.YOUTUBE,
+    name: "The Executioner and Her Way of Life - \"Paper Bouquet\" by Mili",
+    videoId: "https://ladist1.catbox.video/pqnd6x.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
         "OP"
@@ -220,9 +20,9 @@ export const animeEndings = [
     }
   },
   {
-    name: "The Great Ace Attorney 2: Resolve - \"Great Pursuit ~ The Resolve of Naruhodou Ryuunosuke\" by Yasumasa Kitagawa & Hiromitsu Maeba ",
-    videoId: "bAtJnMEUkQs",
-    videoSource: VIDEOSOURCE.YOUTUBE,
+    name: "Anti-Magic Academy: The 35th Test Platoon - \"Embrace Blade\" by Afilia Saga",
+    videoId: "https://ladist1.catbox.video/2merz9.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
         "OP"
@@ -230,9 +30,9 @@ export const animeEndings = [
     }
   },
   {
-    name: "The Legend of Heroes: Trails of Cold Steel - \"Exceed!\" by Yukihiro Jindo (Falcom Sound Team jdk)",
-    videoId: "L2OBpsUK5PU",
-    videoSource: VIDEOSOURCE.YOUTUBE,
+    name: "Unlimited Fafnir - \"FLYING FAFNIR\" by TRUSTRICK",
+    videoId: "https://ladist1.catbox.video/7gfxv5.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
         "OP"
@@ -240,9 +40,9 @@ export const animeEndings = [
     }
   },
   {
-    name: "Wizard of Legend - \"Duel of Legend (Final Boss)\" by Dale North",
-    videoId: "dZDtkS5Qae4",
-    videoSource: VIDEOSOURCE.YOUTUBE,
+    name: "selector infected WIXOSS - \"killy killy JOKER\" by Kanon Wakeshima",
+    videoId: "https://ladist1.catbox.video/c7ga3l.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
         "OP"
@@ -250,9 +50,449 @@ export const animeEndings = [
     }
   },
   {
-    name: "Paper Mario: The Thousand Year Door Remake - \"Vivian\" by Yoshito Sekigawa (Yoshito Hirano), Yuka Tsujiyoko, Koji Kondo",
-    videoId: "6WubRL1mPlQ",
-    videoSource: VIDEOSOURCE.YOUTUBE,
+    name: "Our Last Crusade or the Rise of a New World - \"Against.\" by Kaori Ishihara",
+    videoId: "https://ladist1.catbox.video/7b3ap0.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Destiny of the Shrine Maiden - \"Re-sublimity\" by KOTOKO",
+    videoId: "https://ladist1.catbox.video/n9dpla.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "YU-NO: A girl who chants love at the bound of this world - \"Kono Yo no Hate de Koi o Utau Shoujo\" by Asaka",
+    videoId: "https://ladist1.catbox.video/iy81bc.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Lost Song - \"Utaeba Soko ni Kimi ga Iru kara\" by Konomi Suzuki",
+    videoId: "https://ladist1.catbox.video/5eomj8.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Aikatsu Stars! - \"Start Line!\" by Sena\u30fbRie from AIKATSU\u2606STARS!",
+    videoId: "https://ladist1.catbox.video/7hz0dr.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Asterisk War - \"Brand-new World\" by Shiena Nishizawa",
+    videoId: "https://ladist1.catbox.video/jquoqc.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Elemental Gelade - \"Forever...\" by savage genius",
+    videoId: "https://ladist1.catbox.video/2y4fwc.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Air Gear - \"Chain\" by BACK-ON",
+    videoId: "https://ladist1.catbox.video/ayehyz.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Kyousougiga - \"Koko\" by Tamurapan",
+    videoId: "https://ladist1.catbox.video/ngy51x.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Golden Kamuy - \"Reimei\" by Sayuri\u00d7MY FIRST STORY",
+    videoId: "https://ladist1.catbox.video/r74ii3.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Higurashi no Naku Koro ni Kai - \"Naraku no Hana\" by Eiko Shimamiya",
+    videoId: "https://ladist1.catbox.video/81991h.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Yurikuma Arashi - \"Ano Mori de Matteru\" by Bonjour Suzuki",
+    videoId: "https://ladist1.catbox.video/hewjbo.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Phantom: Requiem for the Phantom - \"KARMA\" by KOKIA",
+    videoId: "https://ladist1.catbox.video/huqtw1.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Magical Girl Lyrical Nanoha A's - \"ETERNAL BLAZE\" by Nana Mizuki",
+    videoId: "https://ladist1.catbox.video/a0tl4q.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Birdy the Mighty Decode:02 - \"kiseki\" by NIRGILIS",
+    videoId: "https://ladist1.catbox.video/opolmf.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Fairy Tail - \"Towa No Kizuna\" by Daisy\u00d7Daisy feat. Another Infinity",
+    videoId: "https://ladist1.catbox.video/opezwg.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Senki Zesshou Symphogear AXZ - \"TESTAMENT\" by Nana Mizuki",
+    videoId: "https://ladist1.catbox.video/txa4ww.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Hunter x Hunter - \"departure!\" by Masatoshi Ono",
+    videoId: "https://ladist1.catbox.video/2nno6y.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Air   - \"Tori no Uta\" by Lia",
+    videoId: "https://ladist1.catbox.video/pg71sc.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Certain Scientific Railgun S - \"sister's noise\" by fripside",
+    videoId: "https://ladist1.catbox.video/qhpece.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Demon Girl Next Door - \"Machikado Tangent\" by shami momo",
+    videoId: "https://ladist1.catbox.video/5kxs3p.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Bloom Into You - \"Kimi ni Furete\" by Riko Azuna",
+    videoId: "https://ladist1.catbox.video/59fvwo.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "TONIKAWA: Over The Moon For You - \"Setsuna no Chikai (feat. Tsukasa Yuzaki)\" by Neko Hacker feat. Akari Kitou",
+    videoId: "https://ladist1.catbox.video/175ys6.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Aikatsu stars! - \"MUSIC of DREAM!!!\" by Sena\u30fbRie\u30fbMiki\u30fbKana from AIKATSU\u2606STARS!",
+    videoId: "https://ladist1.catbox.video/moix30.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Senki Zesshou Symphogear - \"Synchrogazer\" by Nana Mizuki",
+    videoId: "https://ladist1.catbox.video/x1w54c.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Cross Ange: Rondo of Angel and Dragon - \"Shinjitsu no Mokushiroku\" by Yoko Takahashi",
+    videoId: "https://ladist1.catbox.video/v2b3ko.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "World Trigger - \"Ashita no Hikari\" by AAA",
+    videoId: "https://ladist1.catbox.video/w8r3tw.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Nyaruko: Crawling with Love! W - \"Koi wa Chaos no Shimobe Nari\" by Ushiro kara Haiyori-tai G",
+    videoId: "https://ladist1.catbox.video/jwwrba.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Police in a Pod - \"Shiranakya\" by Riko Azuna",
+    videoId: "https://ladist1.catbox.video/2ato8o.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Vlad Love - \"Where you are\" by BlooDye feat. LITTLE (KICK THE CAN CREW)",
+    videoId: "https://ladist1.catbox.video/levq95.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Summer Time Rendering - \"Natsuyume Noisy\" by Asaka",
+    videoId: "https://ladist1.catbox.video/tmz49f.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Fate/kaleid liner Prisma\u2606Illya - \"starlog\" by ChouCho",
+    videoId: "https://ladist1.catbox.video/khidi5.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Hanayamata - \"Hana wa Odore ya Irohaniho\" by Team \"Hanayamata\"",
+    videoId: "https://ladist1.catbox.video/dd7acs.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Strongest Sage With the Weakest Crest - \"Leap of faith\" by fripSide",
+    videoId: "https://ladist1.catbox.video/yahbf6.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Bleach - \"HARUKAZE\" by SCANDAL",
+    videoId: "https://ladist1.catbox.video/4q5hxf.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Yozakura Quartet - \"JUST TUNE\" by savage genius",
+    videoId: "https://ladist1.catbox.video/gf02qz.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "The Fruit of Grisaia - \"Rakuen no Tsubasa\" by Maon Kurosaki",
+    videoId: "https://ladist1.catbox.video/nyxufc.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Big Order - \"DISORDER\" by Yousei Teikoku",
+    videoId: "https://ladist1.catbox.video/r7eyze.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Alderamin on the Sky - \"Tenkyou no Alderamin\" by Kishida Kyoudan &THE Akeboshi Rockets",
+    videoId: "https://ladist1.catbox.video/2ya70j.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Blast of Tempest - \"Daisuki nano ni\" by Kylee",
+    videoId: "https://ladist1.catbox.video/etv7rt.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Strawberry Panic - \"Kuchibiru Daydream\" by Aki Misato",
+    videoId: "https://ladist1.catbox.video/9f0leq.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Build-Divide: #FFFFFF (Code White) - \"Gold\" by EGOIST",
+    videoId: "https://ladist1.catbox.video/lah3jv.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Samurai Champloo - \"battlecry\" by Nujabes feat.Shing02",
+    videoId: "https://ladist1.catbox.video/mr2jw1.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Aikatsu! - \"SHINING LINE*\" by Waka\u30fbFuuri\u30fbYuna from STAR\u2606ANIS",
+    videoId: "https://ladist1.catbox.video/17onnu.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Land of the Lustrous - \"Kyoumen no Nami\" by YURiKA",
+    videoId: "https://ladist1.catbox.video/jh7et0.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
     opts: {
       singer: [
         "OP"
