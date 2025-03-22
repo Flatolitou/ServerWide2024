dc9c2363d66d15f5816f83d54970b20b40b6a5d3
Flatolitou <115586273+Flatolitou@users.noreply.github.com>
2025-03-22 04:41:35 +0100

    Update animeEds.js

--- src/js/data/2024-01-31/misc/animeEds.js
+++ src/js/data/2024-01-31/misc/animeEds.js
@@ -1,8 +1,8 @@
 export const animeEndings = [
   {
-    name: "The Dangers in My Heart Season 2 - Boku wa... by Atarayo",
-    videoId: "https://naedist.animemusicquiz.com/nzt5qc.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
+    name: "Soldier game",
+    videoId: "TA9x6Hpe9F4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
         "OP"
@@ -10,9 +10,9 @@ export const animeEndings = [
     }
   },
   {
-    name: "The Apothecary Diaries - Ambivalent by Uru",
-    videoId: "https://naedist.animemusicquiz.com/86jz91.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
+    name: "Storm in lover",
+    videoId: "7W1QaMBLHls",
+    videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
         "OP"
@@ -20,229 +20,9 @@ export const animeEndings = [
     }
   },
   {
-    name: "The Weakest Tamer Began a Journey to Pick Up Trash - Hate no Nai Tabi by Aina Suzuki",
-    videoId: "https://naedist.animemusicquiz.com/d50ykx.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Heaven Official's Blessing Season 2 - Haru Omoi by krage",
-    videoId: "https://files.catbox.moe/e3808h.mp4",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Jellyfish Can't Swim in the Night - Irodori by KanoeRana",
-    videoId: "https://naedist.animemusicquiz.com/7hkup5.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Train to the End of the World - Eureka by Rokudenashi",
-    videoId: "https://naedist.animemusicquiz.com/4fv3gu.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Unnamed Memory - blan_ by Arika",
-    videoId: "https://naedist.animemusicquiz.com/3wu8xf.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "The Misfit of Demon King Academy II Part 2 - Shingetsu by Tomori Kusunoki",
-    videoId: "https://naedist.animemusicquiz.com/hofkvo.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "NieR:Automata Ver1.1a (Cour 2) - Black Box by LiSA",
-    videoId: "https://naedist.animemusicquiz.com/mrie4l.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "I Parry Everything - AMBITION by Anna Suzuki",
-    videoId: "https://naedist.animemusicquiz.com/s62snj.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Our Last Crusade or the Rise of a New World Season 2 - Para Bellum by Sizuk feat. AYAME(from AliA)",
-    videoId: "https://naedist.animemusicquiz.com/lhfaaw.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "My Hero Academia Season 7 - Rokutousei by ZARAME",
-    videoId: "https://naedist.animemusicquiz.com/ewglme.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Dahlia in Bloom: Crafting a Fresh Start with Magical Tools - Glitter by Marina Horiuchi",
-    videoId: "https://naedist.animemusicquiz.com/a4jgsw.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "[Oshi No Ko] Season 2 - Burning by Hitsujibungaku",
-    videoId: "https://naedist.animemusicquiz.com/ipy1d4.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "That Time I Got Reincarnated as a Slime Season 3 - Miracle soup by MindaRyn",
-    videoId: "https://naedist.animemusicquiz.com/q3j4qf.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Sengoku Youko 2nd Season - Yoru no Manima by Hiroki Nanami",
-    videoId: "https://naedist.animemusicquiz.com/ej8vk8.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Re:Zero: Starting Life in Another World 3rd Season - Reweave by Konomi Suzuki",
-    videoId: "https://naedist.animemusicquiz.com/026i7t.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Blue Box - Same Blue by Official HIGE DANdism",
-    videoId: "https://naedist.animemusicquiz.com/hugcr0.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Nina the Starry Bride - Hoshi no Dengon by Nao Touyama",
-    videoId: "https://naedist.animemusicquiz.com/2ut1bt.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "As a Reincarnated Aristocrat, I'll Use My Appraisal Skill to Rise in the World Season 2 - Familiar by Yuki Tanaka",
-    videoId: "https://naedist.animemusicquiz.com/cfj1e0.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Frieren: Beyond Journey's End - Haru by Yorushika",
-    videoId: "https://naedist.animemusicquiz.com/jkbc3w.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Why Does Nobody Remember Me in This World? - UMBRA by ELFENSJ\u00f3N",
-    videoId: "https://naedist.animemusicquiz.com/r3h6lv.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Villainess Level 99: I May Be the Hidden Boss but I'm Not the Demon Lord - Ray of Light by HaNaTan",
-    videoId: "https://naedist.animemusicquiz.com/erknf4.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "The Irregular at Magic High School Season 3 - Shion no Hanataba o by ASCA",
-    videoId: "https://naedist.animemusicquiz.com/ttk3as.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
-    opts: {
-      singer: [
-        "OP"
-      ]
-    }
-  },
-  {
-    name: "Whisper Me a Love Song - Meritocracy by Laureley",
-    videoId: "https://naedist.animemusicquiz.com/2peg3n.webm",
-    videoSource: VIDEOSOURCE.CATBOX,
+    name: "GUILTY KNIGHT",
+    videoId: "zsly4OzCfuE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
     opts: {
       singer: [
         "OP"
